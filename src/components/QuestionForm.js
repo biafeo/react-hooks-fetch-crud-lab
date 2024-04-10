2bbec865b7c33de3d332cd57cd4796365acb4c5e
beatriz <beatrizffeo@gmail.com>
2024-04-10 12:37:45 -0600

    lab complete

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function QuestionForm(props) {
+function QuestionForm({ onAddQuestion }) {
   const [formData, setFormData] = useState({
     prompt: "",
     answer1: "",
@@ -9,17 +9,42 @@ function QuestionForm(props) {
     answer4: "",
     correctIndex: 0,
   });
-
   function handleChange(event) {
+    const { name, value } = event.target;
     setFormData({
       ...formData,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
   }
-
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const questionObj = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: parseInt(formData.correctIndex),
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(questionObj),
+    })
+      .then((r) => r.json())
+      .then((data) => onAddQuestion(data));
+    setFormData({
+      prompt: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+      answer4: "",
+      correctIndex: 0,
+    });
   }
 
   return (
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,25 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+  async function handleDelete() {
+    try {
+      await fetch(`http://localhost:4000/questions/${id}`, {
+        method: "DELETE",
+      });
+      onDeleteQuestion(id);
+    } catch (error) {
+      console.error("Failed to delete question:", error);
+    }
+  }
+  const options = answers
+    ? answers.map((answer, index) => (
+        <option key={index} value={index}>
+          {answer}
+        </option>
+      ))
+    : [];
 
   return (
     <li>
@@ -17,7 +29,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
